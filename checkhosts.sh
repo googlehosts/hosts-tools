c42704707f5b53843fdd0e4c1171e3f2b2274a08
Andy Deng <theandy.deng@gmail.com>
2016-09-28 20:23:17 +0800

    travis: improve the detection mode
    由于hosts检测脚本有很多不必要的判断/操作，且依赖 dos2unix 的高版本。
    因此，重新修正检测方式，使用 file 替代 dos2unix 来侦测 DOS 换行符，
    取消设置环境变量才检验日期和格式的限制。
    
    同时也精简了格式化hosts的脚本。
    
    Signed-off-by: Andy Deng <theandy.deng@gmail.com>

--- checkhosts.sh
+++ checkhosts.sh
@@ -5,113 +5,93 @@ LINE_BREAK=0
 FORMAT_BREAK=0
 DATE_BREAK=0
 
-# 1. check line endings
-#
-chk_line() {
-    local ret=$(dos2unix -id "$1" | egrep -o "[0-9]+")
+chk_eol()
+{
+	echo -e "- Check line endings:\n"
 
-    echo -e "1. check line endings:\n"
-
-    if [ "$ret" -ne 0 ]; then
-        echo -e "\033[41mDOS line endings $ret times appeared, " \
-                    "it must be coverted!\033[0m\n\n"
-        LINE_BREAK=1
-    else
-        echo -e "\033[42mAll is well!\033[0m\n\n"
-    fi
+	if file "$1" | grep "CRLF" -q; then
+		echo -e "\033[41mDOS line endings have appeared, " \
+			"it must be coverted now!\033[0m\n\n"
+		LINE_BREAK=1
+	else
+		echo -e "\033[42mAll is well!\033[0m\n\n"
+	fi
 }
 
-# 2. check hosts format, only used if STRICT_HOSTS_FORMAT already set
-#
-chk_format() {
-    local loc="[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+"
-    local in_fmt="[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$(echo -e "\t")[[:alnum:]]\+"
+chk_format()
+{
+	echo -e "- Check hosts format:\n"
 
-    echo -e "2. check hosts format:\n"
+	grep -P "\d+\.\d+\.\d+\.\d+" "$1" > 1.txt
+	grep -P "\d+\.\d+\.\d+\.\d+\t\w+" "$1" > 2.txt
 
-    grep "$loc" "$1" > 1.txt
-    grep "$in_fmt" "$1" > 2.txt
+	if ! diff 1.txt 2.txt > 0.txt; then
+		echo -e "\n\033[41mhosts format mismatch! " \
+			"The following rules should be normalized:\033[0m"
+		cat 0.txt
+		FORMAT_BREAK=1
+	else
+		echo -e "\033[42mAll is well!\033[0m"
+	fi
 
-    diff -q 1.txt 2.txt
-
-    if [ "$?" -ne 0 ]; then
-        echo -e "\n\033[41mhosts format mismatch! " \
-                    "The following rules should be normalized:\033[0m"
-        diff 1.txt 2.txt
-        FORMAT_BREAK=1
-    else
-        echo -e "\033[42mAll is well!\033[0m"
-    fi
-
-    echo -e "\n"
-    rm -f 1.txt 2.txt
+	echo -e "\n"
+	rm -f 0.txt 1.txt 2.txt
 }
 
-# 3. check "Last updated", only used if STRICT_HOSTS_FORMAT already set
-#
-chk_date() {
-    # system date
-    local real_date=$(date +%F)
-    # date in git repo's log
-    local repo_date=$(git log --date=short "$1" | egrep -o "[0-9]+-[0-9]+-[0-9]+" -m 1)
-    # date string in hosts file.
-    local hosts_date=$(egrep -o "[0-9]+-[0-9]+-[0-9]+" "$1")
+chk_date()
+{
+	# get the system date
+	local real_date=$(date +%F)
+	# get the hosts last modified in git log records
+	local repo_date=$(git log --date=short "$1" | grep -Pom1 "\d+-\d+-\d+")
+	# date string in hosts file.
+	local hosts_date=$(grep -Po "\d+-\d+-\d+" "$1")
 
-    echo -e "3. check hosts date:\n"
+	echo -e "- Check hosts date:\n"
 
-    # detect hosts file changes
-    if git diff --exit-code "$1" > /dev/null; then
-        # hosts file isn't changed, compare date with git's log.
-        if [ "$repo_date" != "$hosts_date" ]; then
-            echo -e "\033[41mhosts date mismatch, last modified is $repo_date, " \
-                    "but hosts tells $hosts_date\033[0m\n\n"
-            DATE_BREAK=1
-        else
-            echo -e "\033[42mAll is well!\033[0m\n\n"
-        fi
-    else
-        # hosts file is being editing, and not commit yet.
-        # Compare date with system date.
-        if [ "$real_date" != "$hosts_date" ]; then
-            echo -e "\033[41mhosts date mismatch, last modified is $real_date, " \
-                    "but hosts tells $hosts_date\033[0m\n\n"
-            DATE_BREAK=1
-        else
-            echo -e "\033[42mAll is well!\033[0m\n\n"
-        fi
-    fi
+	# check if hosts file changes
+	if git diff --exit-code "$1" > /dev/null 2>&1; then
+		# hosts file is not changed, compare file's date with git log.
+		if [ "$repo_date" != "$hosts_date" ]; then
+			echo -e "\033[41mhosts date mismatch, last modified " \
+				"is $repo_date, but hosts tells " \
+				"$hosts_date\033[0m\n\n"
+			DATE_BREAK=1
+		else
+			echo -e "\033[42mAll is well!\033[0m\n\n"
+		fi
+	else
+		# hosts file is being editing, and has not been committed.
+		# Compare file's date with the system date.
+		if [ "$real_date" != "$hosts_date" ]; then
+			echo -e "\033[41mhosts date mismatch, last modified " \
+				"is $real_date, but hosts tells " \
+				"$hosts_date\033[0m\n\n"
+			DATE_BREAK=1
+		else
+			echo -e "\033[42mAll is well!\033[0m\n\n"
+		fi
+	fi
 }
 
-#
-# Result
-#
-result () {
-    echo -e "Result:\n"
+result()
+{
+	echo -e "Result (1 = yes, 0 = no):\n"
+	echo "line endings break?      [ $LINE_BREAK ]"
+	echo "hosts format mismatch?   [ $FORMAT_BREAK ]"
+	echo "hosts date mismatch?     [ $DATE_BREAK ]"
 
-    echo -e "line endings break?      $LINE_BREAK (1 = yes, 0 = no)"
-
-    if [ -n "$STRICT_HOSTS_FORMAT" ]; then
-        echo -e "hosts format mismatch?   $FORMAT_BREAK (1 = yes, 0 = no)"
-        echo -e "hosts date mismatch?     $DATE_BREAK (1 = yes, 0 = no)"
-
-        local ret=$(echo -e "$LINE_BREAK $FORMAT_BREAK $DATE_BREAK" \
-            | grep -o "1" | wc -w)
-        exit $ret
-    else
-        exit $LINE_BREAK
-    fi
+	local ret=$(expr $LINE_BREAK + $FORMAT_BREAK + $DATE_BREAK)
+	exit $ret
 }
 
-if [ "$1" = "" ]; then
-    echo -e "\033[41mError, requires an argument!\033[0m"
-    exit -1
+if [ -z "$1" ]; then
+	echo "Usage: $0 [your-hosts-file]"
+	exit -1
 fi
 
-chk_line "$1"
-
-if [ -n "$STRICT_HOSTS_FORMAT" ]; then
-    chk_format "$1"
-    chk_date "$1"
-fi
+chk_eol "$1"
+chk_format "$1"
+chk_date "$1"
 
 result
