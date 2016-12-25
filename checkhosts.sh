374268b9dae5fa2a27d75ce19e0c44e42cf1d4af
Andy Deng <theandy.deng@gmail.com>
2016-12-25 14:37:19 +0800

    checkhosts: make script more readable

--- checkhosts.sh
+++ checkhosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/racaljk/hosts
 
-LINE_BREAK=0
+EOL_BREAK=0
 FORMAT_BREAK=0
 DATE_BREAK=0
 
@@ -12,7 +12,7 @@ chk_eol()
 	if file "$1" | grep -q "CRLF"; then
 		printf "\e[31mDOS line endings have appeared, "
 		printf "it must be coverted now!\e[0m\n\n"
-		LINE_BREAK=1
+		EOL_BREAK=1
 	else
 		printf "\e[32mAll is well!\e[0m\n\n"
 	fi
@@ -24,11 +24,11 @@ chk_format()
 	printf "\e[33;1mCheck hosts format:\e[0m\n"
 
 	# Filter all hosts records.
-	cat "$1" | grep -Pv "^\s*#" | grep -P "(\d+\.){3}\d+" > 1.swp
-	# Trailing whitespace detection.
-	grep -P "[ \t]+$" "$1" >> 1.swp
-	# Filter good records.
-	cat "$1" | grep -Pv "^\s*#" | grep -P "^(\d+\.){3}\d+\t\w" > 2.swp
+	grep -Pv "^\s*#" "$1" | grep -P "(\d+\.){3}\d+" > 1.swp
+	# Detect trailing whitespace.
+	grep -P "\s+$" "$1" >> 1.swp
+	# Filter good hosts records.
+	grep -Pv "^\s*#" "$1" | grep -P "^(\d+\.){3}\d+\t\w" > 2.swp
 
 	if ! diff 1.swp 2.swp > 0.swp; then
 		printf "\e[31mhosts format mismatch! "
@@ -42,10 +42,22 @@ chk_format()
 	rm -f 0.swp 1.swp 2.swp
 }
 
+cmp_date()
+{
+	if [ "$1" != "$2" ]; then
+		printf "\e[31mhosts date mismatch, last modified "
+			printf "is $1, but hosts tells "
+			printf "$2\e[0m\n\n"
+			DATE_BREAK=1
+	else
+		printf "\e[32mAll is well!\e[0m\n\n"
+	fi
+}
+
 chk_date()
 {
 	local sys_date=$(date +%F)
-	local repo_date=$(git log --date=short "$1" |
+	local commit_date=$(git log --date=short "$1" |
 					grep -Pom1 "\d{4}-\d{2}-\d{2}")
 	local in_file=$(grep -Po "\d{4}-\d{2}-\d{2}" "$1")
 
@@ -54,24 +66,10 @@ chk_date()
 	# check if hosts file changes.
 	if git diff --exit-code "$1" &> /dev/null; then
 		# hosts file is not changed.
-		if [ "$repo_date" != "$in_file" ]; then
-			printf "\e[31mhosts date mismatch, last modified "
-			printf "is ${repo_date}, but hosts tells "
-			printf "${in_file}\e[0m\n\n"
-			DATE_BREAK=1
-		else
-			printf "\e[32mAll is well!\e[0m\n\n"
-		fi
+		cmp_date "$commit_date" "$in_file"
 	else
 		# hosts file is being editing, and has not been committed.
-		if [ "$sys_date" != "$in_file" ]; then
-			printf "\e[31mhosts date mismatch, last modified "
-			printf "is $sys_date, but hosts tells "
-			printf "$in_file\e[0m\n\n"
-			DATE_BREAK=1
-		else
-			printf "\e[32mAll is well!\e[0m\n\n"
-		fi
+		cmp_date "$sys_date" "$in_file"
 	fi
 }
 
@@ -84,4 +82,4 @@ chk_eol "$1"
 chk_format "$1"
 chk_date "$1"
 
-exit $(( $LINE_BREAK + $FORMAT_BREAK + $DATE_BREAK ))
+exit $(( $EOL_BREAK + $FORMAT_BREAK + $DATE_BREAK ))
